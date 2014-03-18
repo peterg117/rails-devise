61d2deec866f35953dcf42c64a247afc955de5b7
peterg117 <pgranberg11@gmail.com>
2014-03-18 17:35:14 +0000

    added posts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RailsDevise::Application.routes.draw do
-  root :to => "home#index"
+  resources :posts
+
+  root :to => "posts#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
